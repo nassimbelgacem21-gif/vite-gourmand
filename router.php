67cca89b0fea7907f31ce4807298c55a9327a5ae
nassimbelgacem21-gif <nassim.belgacem21@gmail.com>
2026-05-21 20:53:24 +0200

    fix router tentative

--- router.php
+++ router.php
@@ -1,13 +1,14 @@
 <?php
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$file = __DIR__ . $path;
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$file = __DIR__ . $uri;
 
-if (is_file($file)) {
-    if (str_ends_with($file, '.php')) {
-        require $file;
-    } else {
-        return false;
+if ($uri !== '/' && file_exists($file) && !is_dir($file)) {
+    $ext = pathinfo($file, PATHINFO_EXTENSION);
+    if ($ext === 'php') {
+        include $file;
+        return true;
     }
-} else {
-    require __DIR__ . '/index.html';
+    return false;
 }
+
+include __DIR__ . '/index.html';
